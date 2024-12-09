0fd47f1cc70560a793b3c9982781f4777419daf2
Abdullah-Alshamrani <81327925+Abdullah-Alshamrani@users.noreply.github.com>
2024-12-09 17:18:31 -0500

    Update errorMiddleware.js

--- Node.Js/errorMiddleware.js
+++ Node.Js/errorMiddleware.js
@@ -1,3 +1,8 @@
+//Hussam Alanazi 
+//Database Management Systems
+//12/5/2024
+//Final Project - Health Database System -->
+
 exports.errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode || 500;
     res.status(statusCode).json({
